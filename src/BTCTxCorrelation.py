eb5402ef37aab652d78f40cfde27f4d4e747a3fa
Daniel Konecny <dkonecny97@gmail.com>
2022-04-29 16:23:43 +0200

    Add requirements and main script

---
+++ src/BTCTxCorrelation.py
@@ -0,0 +1,14 @@
+"""
+BTC - Darkmarket Transaction Correlation
+Organisation: Brno University of Technology - Faculty of Information Technology
+Author: Daniel Konecny (xkonec75)
+Date: 29. 4. 2022
+"""
+
+
+def main():
+    print("Helou")
+
+
+if __name__ == "__main__":
+    main()
